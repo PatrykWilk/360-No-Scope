afdace31b3588545d0ca88c1addd600a825f0cbc
Patryk <patryk.dawid.wilk@gmail.com>
2019-01-29 13:07:51 +0000

    update to DB connection

--- _includes/connect_db.php
+++ _includes/connect_db.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "dhawserm_360noscope";
+    $servername = "uk59.siteground.eu";
     $username = "dhawserm_lads";
     $password = "7ehK3p6JEjhAP2Y";
-    $database = "360noscope";
+    $database = "dhawserm_360noscope";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
